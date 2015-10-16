ba8a1117f335ea963c7f99f10a846667a5e01839
Jared Jolton <jared.jolton@gmail.com>
2015-10-16 15:20:03 -0600

    working code, problems w/ time complexity

--- gameoflife.py
+++ gameoflife.py
@@ -2,15 +2,18 @@ from pyprocessing import *
 import random
 
 cells = []
+debug = False
 
 class Cell():
-    def __init__(self,x,y,state):
+    def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.state = state
+        self.state = 0#random.randint(0,30) #dead by default
         self.adjacent = []
+        self.aliveNeighbors = 0
 
-    def getAdjacents(self):
+    def getAdjacent(self):
+        self.aliveNeighbors = 0
         topLeft = None
         midLeft = None
         lowLeft = None
@@ -23,60 +26,107 @@ class Cell():
             if self.x-1 == cell.x:
                 if self.y-1 == cell.y:
                     topLeft = cell
-                if self.y == cell.y:
+                elif self.y == cell.y:
                     midLeft = cell
-                if self.y+1 == cell.y:
+                elif self.y+1 == cell.y:
                     lowLeft = cell
-            if self.x == cell.x:
+            elif self.x == cell.x:
                 if self.y-1 == cell.y:
                     topMid = cell
-                if self.y+1 == cell.y:
+                elif self.y+1 == cell.y:
                     lowMid = cell
-            if self.x+1 == cell.x:
+            elif self.x+1 == cell.x:
                 if self.y-1 == cell.y:
                     topRight = cell
-                if self.y == cell.y:
+                elif self.y == cell.y:
                     midRight = cell
-                if self.y+1 == cell.y:
+                elif self.y+1 == cell.y:
                     lowRight = cell
         self.adjacent = [topLeft,midLeft,lowLeft,topMid,lowMid,topRight,midRight,lowRight]
 
     def changeState(self):
-        alive = 0
-        self.getAdjacents()
-        for cell in self.adjacent:
-            if cell != None:
-                if cell.state == 1:
-                    alive += 1
-        if self.state == 1:
+        adj_alive = 0
+        self.getAdjacent()
+        for item in self.adjacent:
+            if item != None:
+                if item.state == 1:
+                    adj_alive += 1
+        if self.state == 1: #alive
             #underpopulation
-            if alive < 2:
+            if adj_alive < 2:
                 self.state = 0
             #overcrowding
-            elif alive > 3:
+            elif adj_alive > 3:
                 self.state = 0
             #alive...for now
             else:
                 self.state = 1 #unnecessary line
-        else:
+        else: #dead
             #reproduction
-            if alive == 3:
+            if adj_alive == 3:
                 self.state = 1
+
+def change_states():
+    for cell in cells:
+        cell.getAdjacent()
+        for item in cell.adjacent:
+            if item != None:
+                if item.state == 1:
+                    cell.aliveNeighbors += 1
+    for cell in cells:
+        if cell.state == 1:
+            #underpopulation
+            if cell.aliveNeighbors < 2:
+                cell.state = 0
+            #overcrowding
+            elif cell.aliveNeighbors > 3:
+                cell.state = 0
+            #alive...for now
+            else:
+                cell.state = 1 #unnecessary line
+        else: #dead
+            #reproduction
+            if cell.aliveNeighbors == 3:
+                cell.state = 1
+
 
 def setup():
     #print "ran setup"
-    size(800,800)
-    #ellipseMode(CENTER)
+    size(1280,800,fullscreen=True)
     noStroke()
-    for x in range(50):
-        for y in range(50):
+    alive = 0
+    for x in range(1,51):
+        for y in range(1,51):
             #randState = random.randint(0,1)
             #newCell = Cell(x,y,randState)
-            newCell = Cell(x,y,0)
+            newCell = Cell(x,y)
+            if y == 23:
+                if x == 25:
+                    newCell.state = 1
+                    alive += 1
+            elif y == 24:
+                if x == 23:
+                    newCell.state = 1
+                    alive += 1
+                elif x == 25:
+                    newCell.state = 1
+                    alive += 1
+            elif y == 25:
+                if x == 24:
+                    newCell.state = 1
+                    alive += 1
+                elif x == 26:
+                    newCell.state = 1
+                    alive += 1
+            elif y == 26:
+                if x == 24:
+                    newCell.state = 1
+                    alive += 1
             cells.append(newCell)
+    #print alive
 
     #setup animation
-
+    '''
     for cell in cells:
         if cell.x == 25:
             if cell.y == 26:
@@ -113,7 +163,6 @@ def setup():
             if cell.x == 17:
                 cell.state = 1
 
-
     for cell in cells:
         if cell.y == 23:
             if cell.x == 23:
@@ -146,7 +195,6 @@ def setup():
                 cell.state = 1
             elif cell.x == 27:
                 cell.state = 1
-    
 
     for cell in cells:
         if cell.y == 24:
@@ -164,28 +212,51 @@ def setup():
                 cell.state = 1
             if cell.x == 26:
                 cell.state = 1
+    '''
 
 def draw():
     #print "ran draw"
-    fill(200,50)
-    rect(0,0,500,500)
-    fill(0)
-    alive = 0
-    for cell in cells:
-        if cell.state == 0: #dead
-            fill(255,255,255)
-            rect(cell.x*16,cell.y*16,16,16)
-        else: #alive
-            alive += 1
-            rand1 = random.randint(0,255)
-            rand2 = 80#random.randint(0,255)
-            rand3 = 150#random.randint(0,255)
-            fill(rand1,rand2,rand3)
-            #fill(0,0,0)
-            rect(cell.x*16,cell.y*16,16,16)
-        #update cell state
-        cell.changeState()
-    #print "finished draw"
-    #print alive
+    if debug == True: #maintain initial state for debugging
+        #fill(200,50)
+        fill(255)
+        rect(0,0,1280,800)
+        fill(0)
+        draw_alive = 0
+        for cell in cells:
+            if cell.state == 0: #dead
+                fill(255,255,255)
+                rect(cell.x*16,cell.y*16,16,16)
+            else: #alive
+                draw_alive += 1
+                rand1 = random.randint(0,255)
+                rand2 = 80#random.randint(0,255)
+                rand3 = 150#random.randint(0,255)
+                fill(rand1,rand2,rand3)
+                #fill(0,0,0)
+                rect(cell.x*16,cell.y*16,16,16)
+        #print alive
+    else: #game of life
+        #fill(200,50)
+        fill(255)
+        rect(0,0,1280,800)
+        fill(0)
+        draw_alive = 0
+        for cell in cells:
+            if cell.state == 0: #dead
+                fill(255,255,255)
+                rect(cell.x*16,cell.y*16,16,16)
+            else: #alive
+                draw_alive += 1
+                rand1 = random.randint(0,55)
+                rand2 = 80#random.randint(0,255)
+                rand3 = 150#random.randint(0,255)
+                fill(rand1,rand2,rand3)
+                #fill(0,0,0)
+                rect(cell.x*16,cell.y*16,16,16)
+        #print "finished draw"
+        change_states()
+        #for cell in cells:
+        #    cell.changeState()
+        #print draw_alive
 
 run()
